3eadc3f361b68686d7d9acab4cd568f9c560287a
Anurag <anuragsonkar053@gmail.com>
2024-09-22 09:43:54 +0530

    error-page-setup

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import { Routes , Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Layout from './components/Layout'
+import Error from './pages/Error'
 function App() {
   return (
     <Routes>
       <Route path='/' Component={Layout}>
         <Route index Component={Home}/>
+        <Route path="*" element={<Error />} />
 
       </Route>
     </Routes>
